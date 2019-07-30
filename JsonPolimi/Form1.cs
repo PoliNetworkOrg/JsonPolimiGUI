387bc2b02c317fdb5034bf6fe72e59774e7b9995
Federico Armellini <armellini.federico@gmail.com>
2019-07-30 09:37:28 +0200

    better convert string in date time

--- JsonPolimi/Form1.cs
+++ JsonPolimi/Form1.cs
@@ -220,6 +220,42 @@ namespace JsonPolimi
             if (data == "null")
                 return null;
 
+            if (data.Contains("T"))
+            {
+                //2019-07-27T17:13:23.5409603+02:00
+                var a1 = data.Split('T');
+
+                //2019-07-27
+                var a2 = a1[0].Split('-');
+
+                //17:13:23.5409603+02:00
+                var a3 = a1[1].Split('+');
+
+                //17:13:23.5409603
+                var a4 = a3[0].Split('.');
+
+                //17:13:23
+                var a5 = a4[0].Split(':');
+
+                return new DateTime(Convert.ToInt32(a2[0]), Convert.ToInt32(a2[1]), Convert.ToInt32(a2[2]), Convert.ToInt32(a5[0]), Convert.ToInt32(a5[1]), Convert.ToInt32(a5[2]));
+            }
+
+            if (data.Contains("."))
+            {
+                //2019-07-29 18:26:55.034083
+                data = data.Split('.')[0];
+
+                var b1 = data.Split(' ');
+
+                //27/07/2019
+                var b2 = b1[0].Split('-');
+
+                //11:42:24
+                var b3 = b1[1].Split(':');
+
+                return new DateTime(Convert.ToInt32(b2[0]), Convert.ToInt32(b2[1]), Convert.ToInt32(b2[2]), Convert.ToInt32(b3[0]), Convert.ToInt32(b3[1]), Convert.ToInt32(b3[2]));
+            }
+
             //27/07/2019 11:42:24
             var s1 = data.Split(' ');
 
